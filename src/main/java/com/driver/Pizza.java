e476d834e15ea0d7c5758dfd91ff55ccfd7e49d1
mahima <mahimajhade123@gmail.com>
2022-12-13 15:47:35 +0530

    plizzaBill

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -12,23 +12,38 @@ public class Pizza {
     }
 
     public int getPrice(){
+        if(isVeg==true){
+            this.price=300;
+        }else {
+            this.price=400;
+        }
+
         return this.price;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        if(isVeg==true) {
+            price = price + 80;
+        }
+        else{
+            price=price+120;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        price=price+70;
     }
 
     public void addTakeaway(){
         // your code goes here
+        price=price+20;
     }
 
     public String getBill(){
         // your code goes here
+      int  bill=price;
         return this.bill;
     }
 }
